6da8180702954d1af997f2e0980b6108d8e62f42
Vennictus <ishjaap.singh07@gmail.com>
2025-08-28 23:52:57 +0530

    Question 3

--- assignment1.cpp
+++ assignment1.cpp
@@ -101,34 +101,53 @@
 //Question 2
 
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int main() {
-    int arr[100], size;
-    cout << "Enter number of elements: ";
-    cin >> size;
-    cout << "Enter elements: ";
-    for (int i = 0; i < size; i++)
-        cin >> arr[i];
+// int main() {
+//     int arr[100], size;
+//     cout << "Enter number of elements: ";
+//     cin >> size;
+//     cout << "Enter elements: ";
+//     for (int i = 0; i < size; i++)
+//         cin >> arr[i];
 
-    for (int i = 0; i < size; i++) {
-        for (int j = i + 1; j < size; ) {
-            if (arr[i] == arr[j]) {
-                for (int k = j; k < size - 1; k++)
-                    arr[k] = arr[k + 1];
-                size--;
-            } else {
-                j++;
-            }
-        }
-    }
+//     for (int i = 0; i < size; i++) {
+//         for (int j = i + 1; j < size; ) {
+//             if (arr[i] == arr[j]) {
+//                 for (int k = j; k < size - 1; k++)
+//                     arr[k] = arr[k + 1];
+//                 size--;
+//             } else {
+//                 j++;
+//             }
+//         }
+//     }
 
-    cout << "Array after removing duplicates: ";
-    for (int i = 0; i < size; i++)
-        cout << arr[i] << " ";
-    cout << endl;
+//     cout << "Array after removing duplicates: ";
+//     for (int i = 0; i < size; i++)
+//         cout << arr[i] << " ";
+//     cout << endl;
+//     return 0;
+// }
+
+// ===================================================================================================
+
+// Question 3
+
+
+//The Program
+
+#include <cstdio>
+
+int main()
+{
+    int i;
+    int arr[5] = {1};
+    for (i = 0; i < 5; i++)
+        printf("%d", arr[i]);
     return 0;
 }
 
-// ===================================================================================================
+//Output
+// 10000
